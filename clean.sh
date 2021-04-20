ed053cd397edcfd7194be7fa608fdfae74889c69
Lana Lebedeva <58044657+LanaLebedeva@users.noreply.github.com>
2021-04-20 06:58:24 +0300

    clean on Mac (school-21)

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+rm -rf ~/Library/Caches/* -y
+rm -rf ~/Library/42_cache
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
